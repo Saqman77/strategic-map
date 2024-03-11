23ff69138a9795d7d83ac921253d6c3a42e20e0a
Dinesh Kumar <114287171+kumardinesh21@users.noreply.github.com>
2024-03-11 17:44:28 +0500

    Adding btn images

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-import { Application, Graphics } from "pixi.js";
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: 0x23395d,
+});
 
-(async () => {
-  // const Application = PIXI.Application;
-  const app = new Application();
-  await app.init({ backgroundColor: 0x23395d, resizeTo: window, antialias: true });
+document.body.appendChild(app.view);
+// Load the first image
+const imageTexture1 = PIXI.Texture.from("./images/buttonminus.png"); // Replace with the path to your first image
+const imageSprite1 = new PIXI.Sprite(imageTexture1);
+imageSprite1.x = 62;
+imageSprite1.y = 500;
+imageSprite1.anchor.set(0.5);
+app.stage.addChild(imageSprite1);
 
-  // Append the application canvas to the document body
-  document.body.appendChild(app.canvas);
-
-
-  //   WE use absolute for complete window for not occur scrolldown
-  app.canvas.style.position = "absolute";
-  app.canvas.style.top = 0;
-  app.canvas.style.left = 0;
-
-
-  //  shapes are store in PIXI.Graphics
-  // const Graphics = Graphics;
-  // const rectangle = new Graphics();
-  // const circle = new Graphics();
-
-
-
-  // rectangle
-  // .setStrokeStyle(2, 0x000000)
-  // .rect(200, 200, 100, 120)
-  // .fill(0xaa33bb);
-  // app.stage.addChild(rectangle);
-
-  // circle
-  //   .setStrokeStyle(2, 0x2bdb8a)
-  //   .circle(200, 400, 50)
-  //   .fill(0x464646);
-
-  // app.stage.addChild(circle);
-})();
+// Load the second image
+const imageTexture2 = PIXI.Texture.from("./images/buttonplus.png"); // Replace with the path to your second image
+const imageSprite2 = new PIXI.Sprite(imageTexture2);
+imageSprite2.x = 130;
+imageSprite2.y = 500;
+imageSprite2.anchor.set(0.5);
+app.stage.addChild(imageSprite2);
